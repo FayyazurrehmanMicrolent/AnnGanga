1dc00f1eb5d413a704e2b654f775d58d48f03183
FayyazurrehmanMicrolent <fayyazur.microlent@gmail.com>
2025-12-09 16:41:48 +0530

    Implement

--- lib/dietary.ts
+++ lib/dietary.ts
@@ -1,35 +1,11 @@
-// dietaryIcons.js
-import {
-  Leaf,
-  Vegan,
-  WheatOff,
-  CandyOff,
-  Dumbbell,
-  BadgeCheck,
-  Ban
-} from "lucide-react";
-
-export const DIETARY_OPTIONS = [
-  { label: "Vegetarian", icon: Leaf },
-  { label: "Vegan", icon: Vegan },
-  { label: "Gluten-Free", icon: WheatOff },
-  { label: "Sugar-Free", icon: CandyOff },
-  { label: "High Protein", icon: Dumbbell },
-  { label: "Organic", icon: BadgeCheck },
-  { label: "Preservative-Free", icon: Ban },
+export const DIETARY = [
+  'Vegetarian',
+  'Vegan',
+  'Gluten-Free',
+  'Sugar-Free',
+  'High Protein',
+  'Organic',
+  'Preservative-Free',
 ];
 
-export default DIETARY_OPTIONS;
-
-
-// export const DIETARY = [
-//   'Vegetarian',
-//   'Vegan',
-//   'Gluten-Free',
-//   'Sugar-Free',
-//   'High Protein',
-//   'Organic',
-//   'Preservative-Free',
-// ];
-
-// export default DIETARY;
+export default DIETARY;
